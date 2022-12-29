ba589443ba583944e0a65d013ccdf1ad8ba7c09e
william <william_swl@163.com>
2022-12-29 23:19:37 +0800

    feat: more number functions

--- R/numbers.R
+++ R/numbers.R
@@ -10,7 +10,26 @@
 #' @examples round_string(1.1, 2)
 round_string <- function(x, digits = 2) {
   x <- as.double(x)
-  format(round(x, digits), nsmall = digits)
+  formatC(x, digits=digits, format='f')
+}
+
+#' from float number to fixed significant digits character
+#'
+#' @param x number
+#' @param digits hold n significant digits
+#'
+#' @return character
+#' @export
+#'
+#' @examples signif_string(1.1, 2)
+signif_string <- function(x, digits = 2) {
+  if (digits <= 0) {
+    stop('Significant digits should be larger than 0!')
+  }
+  x <- as.double(x)
+  formatC(x, digits=digits, format='g', flag='#') %>%
+    ifelse(stringr::str_detect(., '\\.$'),
+           stringr::str_replace(., '\\.', ''), .)
 }
 
 
@@ -19,7 +38,7 @@ round_string <- function(x, digits = 2) {
 #' @param x number
 #' @param digits hold n digits after the decimal point
 #'
-#' @return percent value of x
+#' @return percent character of x
 #' @export
 #'
 #' @examples float_to_percent(0.12)
@@ -29,3 +48,22 @@ float_to_percent <- function(x, digits = 2) {
     round_string(digits) %>%
     stringr::str_c("%")
 }
+
+#' from percent number to float number
+#'
+#' @param x percent number character
+#' @param digits hold n digits after the decimal point
+#'
+#' @return float character of x
+#' @export
+#'
+#' @examples percent_to_float('12%')
+percent_to_float <- function(x, digits = 2) {
+  if (str_detect(x, '^[-\\d\\.]+%$')) {
+    x <- stringr::str_replace(x, '%', '') %>% as.double
+    round_string(x / 100, digits)
+  } else {
+    stop('Not a percent number character!')
+  }
+}
+
--- tests/testthat/test-numbers.R
+++ tests/testthat/test-numbers.R
@@ -1,11 +1,23 @@
 test_that("round_string", {
   expect_identical(round_string(1.1, 2), "1.10")
-  expect_identical(round_string(1.104, 2), "1.10")
+  expect_identical(round_string(1.1041, 2), "1.10")
+  expect_identical(round_string(1.1051, 2), "1.11")
   expect_identical(round_string(10, 2), "10.00")
   expect_identical(round_string(-9, 2), "-9.00")
   expect_identical(round_string(-9.213, 2), "-9.21")
   expect_identical(round_string(-0.523, 0), "-1")
   expect_identical(round_string("-0.5237", 3), "-0.524")
+})
+
+test_that("signif_string", {
+  expect_identical(signif_string(1.1, 2), "1.1")
+  expect_identical(signif_string(1.14, 2), "1.1")
+  expect_identical(signif_string(1.151, 2), "1.2")
+  expect_identical(signif_string(10, 2), "10")
+  expect_identical(signif_string(-9, 2), "-9.0")
+  expect_identical(signif_string(-9.213, 2), "-9.2")
+  expect_error(signif_string(-0.523, 0))
+  expect_identical(signif_string("-0.5237", 3), "-0.524")
 })
 
 
@@ -14,4 +26,11 @@ test_that("float_to_percent", {
   expect_identical(float_to_percent(0, 2), "0.00%")
   expect_identical(float_to_percent(1.37, 2), "137.00%")
   expect_identical(float_to_percent("-12.215", 0), "-1222%")
+})
+
+test_that("percent_to_float", {
+  expect_identical(percent_to_float('12%'), "0.12")
+  expect_identical(percent_to_float('12%', 4), "0.1200")
+  expect_identical(percent_to_float("-12.251%", 3), "-0.123")
+  expect_error(percent_to_float(1.37))
 })
