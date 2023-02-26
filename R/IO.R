8f8b8c26b996ef6436c4b32abc90b40d3128241c
william <william_swl@163.com>
2023-02-26 12:09:01 +0800

    feat: write_excel add mult-sheet support, close #13

--- R/IO.R
+++ R/IO.R
@@ -1,20 +1,41 @@
 #' write a tibble into an excel file
 #'
+#' @param df tibble or a list of tibbles
 #' @param filename the output filename
-#' @param sheetname the sheet name, `sheet1` as default
+#' @param sheetname the name of sheets, `sheet1` as default
 #' @param creator creator
-#' @param df tibble
 #'
 #' @return return status
 #' @export
 #'
 #' @examples write_excel(mini_diamond, "mini_diamond.xlsx")
 write_excel <- function(df, filename, sheetname = "sheet1", creator = "") {
-  wb <- openxlsx::createWorkbook(creator = creator)
-  openxlsx::addWorksheet(wb, sheetName = sheetname)
-  openxlsx::writeData(wb, sheet = sheetname, x = df)
   if (!stringr::str_detect(filename, "\\.xlsx*$")) {
     stop("File name should have xlsx/xls suffix!")
   }
+
+  wb <- openxlsx::createWorkbook(creator = creator)
+
+  addsheet <- function(wb, name, table) {
+    openxlsx::addWorksheet(wb, sheetName = name)
+    openxlsx::writeData(wb, sheet = name, x = table)
+  }
+
+  # data.frame is list, but list is not data.frame
+  if (is.data.frame(df) == TRUE) {
+    addsheet(wb, sheetname, df)
+  } else if (is.list(df) == TRUE) {
+    # sheet names
+    if (sheetname == "sheet1") {
+      sheetname <- stringr::str_c("sheet", seq_along(df))
+    }
+
+    if (length(df) != length(sheetname)) {
+      stop("Different numbers between dataframe and sheet names!")
+    }
+
+    purrr::walk2(sheetname, df, ~ addsheet(wb, .x, .y))
+  }
+
   openxlsx::saveWorkbook(wb, filename, overwrite = TRUE)
 }
--- tests/testthat/test-IO.R
+++ tests/testthat/test-IO.R
@@ -5,9 +5,24 @@ test_that("write_excel", {
     return(path)
   }
 
-  announce_snapshot_file("mini_diamond.xlsx")
-  path <- write_excel_path(mini_diamond)
-  expect_snapshot_file(path, "mini_diamond.xlsx", compare = compare_file_text)
+  announce_snapshot_file("1sheet.xlsx")
+  path <- write_excel_path(mini_diamond[1:5, ])
+  expect_snapshot_file(path, "1sheet.xlsx", compare = compare_file_text)
+})
+
+test_that("write_excel multi sheets", {
+  write_excel_path <- function(df_list) {
+    path <- tempfile(fileext = ".xlsx")
+    write_excel(df_list, path)
+    return(path)
+  }
+
+  announce_snapshot_file("2sheets.xlsx")
+  path <- write_excel_path(list(
+    mini_diamond[1:5, 1:3],
+    mini_diamond[6:10, 1:3]
+  ))
+  expect_snapshot_file(path, "2sheets.xlsx", compare = compare_file_text)
 })
 
 
