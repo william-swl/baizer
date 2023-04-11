1f974e5419631e4171cfb23c57aaddd76492948b
william <william_swl@163.com>
2023-04-11 16:08:31 +0800

    fix: fix R-cmd-check

---
+++ R/R-cmd-check.R
@@ -0,0 +1,6 @@
+# fix the note:
+# hist_bins: no visible global function definition for 'between'
+# hist_bins: no visible binding for global variable 'value'
+# hist_bins: no visible binding for global variable 'start'
+# hist_bins: no visible binding for global variable 'end'
+utils::globalVariables(c("between", "value", "start", "end"))
